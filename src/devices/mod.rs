db34dedac71e05cdf05be3f347cebd833f7697c9
Clément Bizeau <clement.bizeau4@gmail.com>
2017-12-30 18:22:52 +0100

    add a layer to manage i2c coms

---
+++ src/devices/mod.rs
@@ -0,0 +1,116 @@
+// Copyright 2017 Clément Bizeau
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! The `devices` module contains all functionnality to access devices
+
+use f3::stm32f30x::i2c1::RegisterBlock;
+
+/// The I2C address mode
+pub enum I2CAddrMode {
+    /// 10 bits adress mode
+    Bits10,
+    /// 7 bits address mode
+    Bits7,
+}
+
+/// I2C device
+///
+/// # Arguments
+///
+/// * `device` The register block of the i2c device
+pub struct I2C<'a> {
+    pub device: &'a RegisterBlock,
+}
+
+impl<'a> I2C<'a> {
+
+    /// Disable i2c device
+    pub fn disable(&self) {
+        self.device.cr1.modify(|_, w| w.pe().clear_bit());
+    }
+
+    /// Enable i2c device
+    pub fn enable(&self) {
+        self.device.cr1.modify(|_, w| w.pe().set_bit());
+    }
+
+    /// Set the address type
+    ///
+    /// #Arguments
+    ///
+    /// * `addr_mode` - the address mode, either 7 bits or 10 bits
+    pub fn set_addr_type(&self, addr_mode: I2CAddrMode) {
+        match addr_mode {
+            I2CAddrMode::Bits10 => self.device.cr2.modify(|_, w| w.add10().set_bit()),
+            I2CAddrMode::Bits7 => self.device.cr2.modify(|_, w| w.add10().clear_bit()),
+        }
+    }
+
+    /// Begins transmission with a slave
+    ///
+    /// #Arguments
+    ///
+    /// * `addr` the address of the salve
+    pub fn begin(&self, addr: u16) {
+        unsafe {
+            self.device
+                .cr2
+                .modify(|_, w| w.sadd1().bits(addr as u8).autoend().clear_bit());
+        }
+    }
+
+    /// Ends transmission
+    pub fn end(&self) {
+        self.device.cr2.modify(|_, w| w.stop().set_bit());
+    }
+
+    /// Write a byte to a slave
+    pub fn write(&self, byte: &u8) {
+        unsafe {
+            self.device.cr2.modify(|_, w| w.rd_wrn().clear_bit().nbytes().bits(1));
+            self.device.txdr.modify(|_, w| w.bits(*byte as u32));
+            self.device.cr2.modify(|_, w| w.start().set_bit());
+        }
+    }
+
+    /// Request read to a slave
+    pub fn request_read(&self) {
+        self.device.cr2.modify(|_, w| w.rd_wrn().set_bit().start().set_bit());
+    }
+
+    /// Read the rx buffer
+    pub fn rx_read(&self) -> u8 {
+        self.device.rxdr.read().rxdata().bits()
+    }
+
+    /// Is a transfer completed
+    pub fn transfer_completed(&self) -> bool {
+        self.device.isr.read().tc().bit_is_set()
+    }
+
+    /// Is the RX buffer full
+    pub fn rx_buffer_full(&self) -> bool {
+        self.device.isr.read().rxne().bit_is_set()
+    }
+
+    /// Is the communication stopped
+    pub fn stopped(&self) -> bool {
+        self.device.isr.read().stopf().bit_is_set()
+    }
+
+    /// clear the sopped flag
+    pub fn clear_stop(&self) {
+        self.device.icr.write(|w| w.stopcf().set_bit());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,24 @@
 #![feature(proc_macro)]
 #![no_std]
 
-extern crate f3;
 extern crate cortex_m;
 extern crate cortex_m_rtfm as rtfm;
 extern crate cortex_m_semihosting as semihosting;
+extern crate f3;
+
+mod devices;
 
 use core::fmt::Write;
 use rtfm::{app, Threshold};
 use semihosting::hio;
-use cortex_m::peripheral::SystClkSource;
+use devices::{I2C, I2CAddrMode};
 
 const GPIO_FREQ_HIGH: u8 = 0x3;
 const GPIO_PULLUP: u8 = 0x1;
 const GPIO_AF_4: u8 = 0x4;
 
 const ADDR_MAGNETIC_SENSOR: u8 = 0x1E;
-const MAGNETIC_REG_CRA_REG_M: u32 = 0x0;
+const MAGNETIC_REG_CRA_REG_M: u8 = 0x0;
 //const MAGNETIC_REG_OUT_X_H_M: u32 = 0x03;
 //const MAGNETIC_REG_IRA_M: u32 = 0x03;
 //const TEMP_REG_OUT_H_M: u32 = 0x31;
@@ -51,19 +53,27 @@ app! {
 }
 
 fn init(p: init::Peripherals) {
-    p.SYST.set_clock_source(SystClkSource::Core);
-    p.SYST.set_reload(9_000_000);
-    p.SYST.enable_interrupt();
-    p.SYST.enable_counter();
-
     // Configure gpiob : PB6 & PB7
     p.RCC.ahbenr.modify(|_, w| w.iopben().enabled());
-    p.GPIOB.moder.modify(|_, w| w.moder6().alternate().moder7().alternate());
-    p.GPIOB.otyper.modify(|_, w| w.ot6().set_bit().ot7().set_bit());
+    p.GPIOB
+        .moder
+        .modify(|_, w| w.moder6().alternate().moder7().alternate());
+    p.GPIOB
+        .otyper
+        .modify(|_, w| w.ot6().set_bit().ot7().set_bit());
     unsafe {
-        p.GPIOB.ospeedr.modify(|_, w| {w.ospeedr6().bits(GPIO_FREQ_HIGH).ospeedr7().bits(GPIO_FREQ_HIGH)});
-        p.GPIOB.pupdr.modify(|_, w| w.pupdr6().bits(GPIO_PULLUP).pupdr7().bits(GPIO_PULLUP));
-        p.GPIOB.afrl.modify(|_, w| w.afrl6().bits(GPIO_AF_4).afrl7().bits(GPIO_AF_4));
+        p.GPIOB.ospeedr.modify(|_, w| {
+            w.ospeedr6()
+                .bits(GPIO_FREQ_HIGH)
+                .ospeedr7()
+                .bits(GPIO_FREQ_HIGH)
+        });
+        p.GPIOB
+            .pupdr
+            .modify(|_, w| w.pupdr6().bits(GPIO_PULLUP).pupdr7().bits(GPIO_PULLUP));
+        p.GPIOB
+            .afrl
+            .modify(|_, w| w.afrl6().bits(GPIO_AF_4).afrl7().bits(GPIO_AF_4));
     }
 
     // Reset i2c1
@@ -73,39 +83,53 @@ fn init(p: init::Peripherals) {
     p.RCC.cfgr3.modify(|_, w| w.i2c1sw().clear_bit());
     p.RCC.apb1enr.modify(|_, w| w.i2c1en().enabled());
 
+    let i2c_1 = I2C { device: p.I2C1 };
+
     // Disable i2c1 peripheral
-    p.I2C1.cr1.modify(|_, w| w.pe().clear_bit());
+    i2c_1.disable();
 
     // Configure i2c1
     unsafe {
-        p.I2C1.cr1.modify(|_, w| w.anfoff().clear_bit()
-            .dnf().bits(0)
-            .nostretch().clear_bit()
-            .errie().set_bit()
-            .tcie().set_bit()
-            .stopie().set_bit()
-            .nackie().set_bit()
-            .rxie().set_bit()
-            .txie().set_bit());
+        p.I2C1.cr1.modify(|_, w| {
+            w.anfoff()
+                .clear_bit()
+                .dnf()
+                .bits(0)
+                .nostretch()
+                .clear_bit()
+                .errie()
+                .set_bit()
+                .tcie()
+                .set_bit()
+                .stopie()
+                .set_bit()
+                .nackie()
+                .set_bit()
+                .rxie()
+                .set_bit()
+                .txie()
+                .set_bit()
+        });
         p.I2C1.timingr.modify(|_, w| {
-            w.presc().bits(1)
-                .scll().bits(0x13)
-                .sclh().bits(0xF)
-                .sdadel().bits(0x2)
-                .scldel().bits(0x4)
+            w.presc()
+                .bits(1)
+                .scll()
+                .bits(0x13)
+                .sclh()
+                .bits(0xF)
+                .sdadel()
+                .bits(0x2)
+                .scldel()
+                .bits(0x4)
         });
     }
 
     // Enable i2c1 peripheral and set 7 bit adress mode
-    p.I2C1.cr1.modify(|_, w| w.pe().set_bit());
-    p.I2C1.cr2.modify(|_, w| w.add10().clear_bit());
-    unsafe {
-        // write to slave
-        p.I2C1.cr2.modify(|_, w| w.sadd1().bits(ADDR_MAGNETIC_SENSOR).rd_wrn().clear_bit().autoend().clear_bit());
-        p.I2C1.txdr.modify(|_, w| w.bits(MAGNETIC_REG_CRA_REG_M));
-        p.I2C1.cr2.modify(|_, w| w.nbytes().bits(1));
-        p.I2C1.cr2.modify(|_, w| w.start().set_bit());
-    }
+    i2c_1.enable();
+    i2c_1.set_addr_type(I2CAddrMode::Bits7);
+    // write to slave
+    i2c_1.begin(ADDR_MAGNETIC_SENSOR as u16);
+    i2c_1.write(&MAGNETIC_REG_CRA_REG_M);
 }
 
 fn idle() -> ! {
@@ -115,17 +139,36 @@ fn idle() -> ! {
 }
 
 fn i2c_event(_t: &mut Threshold, r: I2C1_EV_EXTI23::Resources) {
-    writeln!(hio::hstdout().unwrap(), "i2c_event->i2c1::isr = {:x}", r.I2C1.isr.read().bits()).unwrap();
-    if r.I2C1.isr.read().rxne().bit_is_set() { // read rx buffer and stop
-        writeln!(hio::hstdout().unwrap(), "i2c_event->i2c1::rxdr = {:x}", r.I2C1.rxdr.read().bits()).unwrap();
-        r.I2C1.cr2.modify(|_, w| w.stop().set_bit());
-    } else if r.I2C1.isr.read().stopf().bit_is_set() { // stop
-        r.I2C1.icr.write(|w| w.stopcf().set_bit());
-    } else if r.I2C1.isr.read().tc().bit_is_set() { // read on slave
-        r.I2C1.cr2.modify(|_, w| w.rd_wrn().set_bit().start().set_bit());
+    let i2c_1 = I2C { device: r.I2C1 };
+    if i2c_1.rx_buffer_full() {
+        // read rx buffer and stop
+        writeln!(hio::hstdout().unwrap(), "i2c_event->recv").unwrap();
+        writeln!(
+            hio::hstdout().unwrap(),
+            "i2c_event->i2c1::rxdr = {:x}",
+            i2c_1.rx_read()
+        ).unwrap();
+        i2c_1.end();
+    } else if i2c_1.stopped() {
+        // stop
+        writeln!(hio::hstdout().unwrap(), "i2c_event->stop").unwrap();
+        i2c_1.clear_stop();
+    } else if i2c_1.transfer_completed() {
+        // read on slave
+        writeln!(hio::hstdout().unwrap(), "i2c_event->read_on_slave").unwrap();
+        i2c_1.request_read();
     }
+    writeln!(
+        hio::hstdout().unwrap(),
+        "i2c_event->i2c1::isr = {:x}",
+        r.I2C1.isr.read().bits()
+    ).unwrap();
 }
 
 fn i2c_error(_t: &mut Threshold, r: I2C1_ER::Resources) {
-    writeln!(hio::hstdout().unwrap(), "i2c_error->i2c1::isr = {:x}", r.I2C1.isr.read().bits()).unwrap();
+    writeln!(
+        hio::hstdout().unwrap(),
+        "i2c_error->i2c1::isr = {:x}",
+        r.I2C1.isr.read().bits()
+    ).unwrap();
 }
